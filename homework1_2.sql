d61b13e2525a71fd653c456c9e98379d60567681
Дмитрий Тищенко GH <107504096+DmitrTishchenko@users.noreply.github.com>
2023-02-10 15:44:20 +0300

    Lesson1
    homework 1_1

---
+++ homework1_2.sql
@@ -0,0 +1,44 @@
+DROP DATABASE IF EXISTS homework_1;
+CREATE DATABASE homework_1;
+USE homework_1;
+
+-- мобильные телефоны
+CREATE TABLE mobile_phones(
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+	product_name VARCHAR(45) NOT NULL, 
+	manufacturer VARCHAR(45) NOT NULL,
+    	product_count VARCHAR(45) NOT NULL,
+	price BIGINT UNSIGNED
+);
+
+-- наполнение
+INSERT INTO mobile_phones (product_name, manufacturer, product_count,price)
+VALUES 
+('iPhone X', 'Apple', 3, 76000),
+('iPhone 8', 'Apple', 2, 51000),
+('Galaxy S9', 'Samsung', 2, 56000),
+('Galaxy S8', 'Samsung', 1, 41000),	
+('P20 Pro', 'Huawei', 5, 36000);
+
+
+-- задание 2: Выведите название, производителя и цену для товаров, количество которых превышает 2
+SELECT product_name,manufacturer,price FROM mobile_phones
+WHERE product_count <= '2';
+
+-- задание 3: Выведите весь ассортимент товаров марки “Samsung”
+SELECT * FROM mobile_phones
+WHERE manufacturer='Samsung';
+
+-- задание 4:(по желанию) С помощью регулярных выражений найти:
+--           Товары, в которых есть упоминание "Iphone"
+SELECT * FROM mobile_phones 
+WHERE product_name LIKE 'iPhone%';
+--           Товары, в которых есть упоминание"Samsung"
+SELECT * FROM mobile_phones 
+WHERE manufacturer LIKE 'Samsung%';
+--           Товары, в которых есть ЦИФРЫ
+SELECT * FROM mobile_phones 
+WHERE product_name REGEXP '[0-9]';
+--           Товары, в которых есть ЦИФРА "8"
+SELECT * FROM mobile_phones 
+WHERE product_name REGEXP '[8]';
